699e76752bc1851dcf581ecc6557822676a9e235
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2025-09-05 22:37:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 
-Messstellen = pd.read_csv('https://www.umwelt.sachsen.de/umwelt/infosysteme/niwis/weitere/Export_MKZ_Uebersicht.csv')
+Messstellen = pd.read_csv('https://www.umwelt.sachsen.de/umwelt/infosysteme/niwis/weitere/Export_MKZ_Uebersicht.csv',
+                          sep=';',
+                          thousands='.',
+                          decimal=',',
+                          index_col='MKZ')
 
 st.write(Messstellen)
