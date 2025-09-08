0402ca3a7479491b885d5117efbab7add91c9090
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2025-09-08 10:12:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
+import urllib.request
+from pyproj import Transformer
+
+st.set_page_config(layout="wide")
 
 Messstellen = pd.read_csv('https://www.umwelt.sachsen.de/umwelt/infosysteme/niwis/weitere/Export_MKZ_Uebersicht.csv',
                           sep=';',
@@ -11,8 +15,15 @@ Messstellen = pd.read_csv('https://www.umwelt.sachsen.de/umwelt/infosysteme/niwi
                           index_col='MKZ')
 c1, c2 = st.columns([0.3,0.7])
 
+transformer = Transformer.from_crs("EPSG:25833", "EPSG:4326")
+
+lat, lon = transformer.transform(Messstellen.RW_ETRS89, Messstellen.HW_ETRS89)
+
+Messstellen['lat'] = lat
+Messstellen['lon'] = lon
+
 with c1:
   st.write(Messstellen)
 
 with c2:
-  st.map(data=Messstellen, latitude='RW_ETRS89', longitude='HW_ETRS89')
+  st.map(data=Messstellen)
