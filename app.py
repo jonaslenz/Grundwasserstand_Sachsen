ad25d3b1996516d9c0b71b9d45e806dde0f4f0e3
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2026-01-14 15:28:35 +0100

    Trendanalyse optional für alle MKZ

--- app.py
+++ app.py
@@ -41,11 +41,6 @@ Mess_GWK = pd.read_csv('./MKZ_GWK.csv',
                       )
 Mess_GWK = Mess_GWK.fillna("na")
 
-if not os.path.isfile('./MKZ_Trend.csv'):
-    Mess_GWK["Trend"] = np.nan
-else:
-    Mess_Trend = pd.read_csv('./MKZ_Trend.csv')
-    Mess_GWK = Mess_GWK.merge(Mess_Trend, on = "MKZ")
 
 Messstellen = Messstellen.merge(Mess_GWK, on = "MKZ")
 
@@ -64,7 +59,7 @@ del lat, lon
 c1, c2 = st.columns([0.4,0.6])
 
 with c1:
-  columns = ['MKZ', 'Erstes_Messdatum', 'Letztes_Messdatum', 'GWK', 'GWK25', 'WRRL', 'RW_ETRS89', 'HW_ETRS89', 'Trend']
+  columns = ['MKZ', 'Erstes_Messdatum', 'Letztes_Messdatum', 'GWK', 'GWK25', 'RW_ETRS89', 'HW_ETRS89']
   df1 = pd.DataFrame(Messstellen, columns=columns)
   df1 = filter_dataframe(df1)
   with st.expander("Auswahliste", expanded=True):
@@ -86,9 +81,7 @@ with c1:
 #  st.write(MKZs)
 #  st.write(Auswahl)
 #  st.write(Messstellen.loc[MKZs, "MKZ"])
-  c1c1, c1c2 = st.columns([0.5,0.5])
-  with c1c1:
-      st.map(data=Auswahl,
+  st.map(data=Auswahl,
              use_container_width=True,
              height=200,
              zoom = 5)
@@ -106,106 +99,159 @@ with c2:
                     horizontal = True)
 
 # MKZs = Messstellen.iloc[1:5,].loc[:,"MKZ"].tolist() # Auswahl Messstellen auserhalb Streamlit
-    for x in MKZs:
-      cacheorload("ExportSN_GWS-Rohdaten_"+x+".csv")
-
-      #dateparse = lambda x: datetime.datetime.strptime(x, '%Y-%m-%d')
-
-      add = pd.read_csv('./cache/ExportSN_GWS-Rohdaten_'+x+'.csv',
-                      sep=';',
-                      thousands='.',
-                      decimal=',',
-       #               parse_dates=["MESSZEITPUNKT"],
-        #              date_parser=dateparse,
-                      )
-      add['MESSZEITPUNKT'] = pd.to_datetime(add['MESSZEITPUNKT'], format='%Y-%m-%d')
-      try:
-        len(alle.index)
-      except NameError:
-        alle = add.copy()
-      else:
-        alle = pd.concat([alle, add])
-
-    fig = px.line(alle, x="MESSZEITPUNKT",y=type, color = "MKZ", height=600)
-
-    if type == "WERT_UNTER_GELAENDE":
-      fig['layout']['yaxis']['autorange'] = "reversed"
-
-    st.plotly_chart(fig)
+    with st.expander("Gangliniendarstellung", expanded=True):
+        for x in MKZs:
+          cacheorload("ExportSN_GWS-Rohdaten_"+x+".csv")
+    
+          #dateparse = lambda x: datetime.datetime.strptime(x, '%Y-%m-%d')
+    
+          add = pd.read_csv('./cache/ExportSN_GWS-Rohdaten_'+x+'.csv',
+                          sep=';',
+                          thousands='.',
+                          decimal=',',
+           #               parse_dates=["MESSZEITPUNKT"],
+            #              date_parser=dateparse,
+                          )
+          add['MESSZEITPUNKT'] = pd.to_datetime(add['MESSZEITPUNKT'], format='%Y-%m-%d')
+          try:
+            len(alle.index)
+          except NameError:
+            alle = add.copy()
+          else:
+            alle = pd.concat([alle, add])
+    
+        fig = px.line(alle, x="MESSZEITPUNKT",y=type, color = "MKZ", height=500)
+    
+        if type == "WERT_UNTER_GELAENDE":
+          fig['layout']['yaxis']['autorange'] = "reversed"
+    
+        st.plotly_chart(fig)
+        alle["pred"] = False
+    #    st.write(alle.head())
 
 # Trendberechnung
+    with st.expander("Trendberechnung", expanded=False):
+        c2c1, c2c2, c2c3, c2c4 = st.columns([0.3,0.3,0.3,0.1])
+        with c2c1:
+            Mindatum = st.date_input("Daten müssen seit mindestens vorliegen:",
+                                 value = datetime.date(2011, 1, 1))
+        with c2c2:
+            Enddatum = st.date_input("Daten müssen bis mindestens vorliegen:",
+                                 value = datetime.date(2024, 1, 1))
+        # Startdatum = datetime.date(1995, 1, 1)
+        # Enddatum = datetime.date(2024, 1, 1)
+        with c2c3:
+            Startdatum = st.date_input("Trend ab:",
+                                   value = datetime.date(1995, 1, 1))
+        with c2c4:    
+           runtrend = st.button("Trend berechnen")
+           runtrendall = st.button("Alle berechnen - keine Grafik, dauert")
+        
+        if runtrend or runtrendall:
+            Messstellen["Trend"] = np.nan
+            first_notice = True
+            if runtrendall:
+                MKZs = Messstellen["MKZ"]
+                counter = 1
+            for z in MKZs:
+                if pd.isna(Messstellen.loc[(Messstellen["MKZ"]==z, "Trend")]).values[0]:
+                # if pd.isna(Messstellen["Trend"][Messstellen["MKZ"]==z]).values[0]:
+                    if first_notice:
+                        st.write("Versuche Trendberechnung für "+str(len(MKZs))+" Messstellen seit "+str(Startdatum)+".")
+                        first_notice = False
+                                   
+                    if ((Messstellen.loc[
+                            Messstellen["MKZ"]==z, "Letztes_Messdatum"] <
+                            pd.to_datetime(Enddatum)).values[0]):
+                        #st.warning("Zeitreihe zu früh zu Ende bei: "+z)
+                        #Messstellen.loc[(Messstellen["MKZ"]==z, "Trend")] = -9999
+                        continue
+                    if ((Messstellen.loc[
+                            Messstellen["MKZ"]==z, "Erstes_Messdatum"] >
+                            pd.to_datetime(Mindatum)).values[0]):
+                        #st.warning("Zeitreihe zu spät begonnen bei: "+z)
+                        #Messstellen.loc[(Messstellen["MKZ"]==z, "Trend")] = -9999
+                        continue
+            # Laden der Datenreihe
+                    cacheorload("ExportSN_GWS-Rohdaten_"+z+".csv")
+                    add = pd.read_csv('./cache/ExportSN_GWS-Rohdaten_'+z+'.csv',
+                                      sep=';',
+                                      thousands='.',
+                                      decimal=',',
+                     #               parse_dates=["MESSZEITPUNKT"],
+                     #              date_parser=dateparse,
+                     )
+                    add['MESSZEITPUNKT'] = pd.to_datetime(add['MESSZEITPUNKT'], format='%Y-%m-%d')
+                    add = add.set_index('MESSZEITPUNKT')
+                    add = add.loc[:,"WERT_UNTER_GELAENDE"]
+                    add = add.dropna()
+                    add = add.loc[add.index>=pd.to_datetime(Startdatum)]
+                    if(add.shape[0]<=0):
+                        #st.warning("Keine Daten bei: "+z)
+                        #Messstellen.loc[(Messstellen["MKZ"]==z, "Trend")] = -9999
+                        continue
+        #            if 
+        
+        # Lineare Regression
+                    x = (add.index - add.index[0]).days.values.reshape(-1, 1) / 365 # /365 ändert die Zeitwerte von Tag auf Jahr
+                    y = add.values                                                  # Die WERT_UNTER_GELAENDE Werte sind in cm
+                    a = linear_model.LinearRegression().fit(x, y)
+                    linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1)
+    
+                    if runtrendall:
+                        if counter % 100 == 0:
+                            st.write(str(counter+ " von "+str(len(MKZs))+" bearbeitet."))
+                    if runtrend:
+                        
+            #            import matplotlib.pyplot as plt
+                        pred = alle.iloc[0:2,].copy()
+            #            st.write(pred)
+                        pred.loc[0,"MKZ"] = z
+                        pred.loc[0,"MESSZEITPUNKT"] = add.index[0]
+                        pred.loc[0,"WERT_UNTER_GELAENDE"] = a.predict([[x.min()]])[0]
+                        pred.loc[0,"pred"] = True
+        
+                        pred.loc[1,"MKZ"] = z
+                        pred.loc[1,"MESSZEITPUNKT"] = add.index[-1]
+                        pred.loc[1,"WERT_UNTER_GELAENDE"] = a.predict([[x.max()]])[0]
+                        pred.loc[1,"pred"] = True
+            #            st.write(pred)
+        
+                        alle = pd.concat([alle, pred])
+                    
+            #            plt.plot(x,y)
+            #            plt.plot([x.min(),x.max()],y_pred)
+            #            plt.text(0,y.min(), a.coef_)
+            #            plt.show()
+    
+        # Berechnung Grimm Strele Trend
+                    Messstellen.loc[(Messstellen["MKZ"]==z, "Trend Grimm Strele")] = -a.coef_ / (y.max()-y.min()) *100 # Anstieg in cm/a / Spannweite der cm --> Grimm-Strele Test
+                    Messstellen.loc[(Messstellen["MKZ"]==z, "Anstieg Regression")] = -a.coef_  # Anstieg in cm/a
+                    Messstellen.loc[(Messstellen["MKZ"]==z, "Spanne")] = y.max()-y.min() # Spannweite der cm
+                    Messstellen.loc[(Messstellen["MKZ"]==z, "Anzahl Werte")] = len(y) # Anzahl der Werte
+                    Turnus = np.median(np.diff([xss for xs in x for xss in xs]))*365
+                    Messstellen.loc[(Messstellen["MKZ"]==z, "Messturnus [Tage] geschätzt")] = Turnus # Anzahl der Werte
+            if runtrend:
+                fig = px.line(alle, x="MESSZEITPUNKT",y="WERT_UNTER_GELAENDE", color = "MKZ", height=500)
+                fig['layout']['yaxis']['autorange'] = "reversed"
+                st.plotly_chart(fig)
+    
+            #    Darstellung Histogram und Prozent
+                trends = Messstellen["Trend"].copy()
+                trends = trends.dropna()
+                fig2 = px.histogram(Messstellen, x="Trend", height=250)
+                st.plotly_chart(fig2)
+                if trends.shape[0]>=1:
+                    st.write("Anteil negativer Trend: "+
+                             str(round(len(trends[trends<=-2]) / len(trends) * 100))+
+                             "% von "+
+                             str(len(trends))+" Messstellen.")
+                st.write(Messstellen)
 
-    Startdatum = st.date_input("Trend ab:",
-                               value = datetime.date(1995, 1, 1))
-    Enddatum = st.date_input("Daten bis mindestens:",
-                             value = datetime.date(2024, 1, 1))
-    # Startdatum = datetime.date(1995, 1, 1)
-    # Enddatum = datetime.date(2024, 1, 1)
-    if st.button("Berechnete Trends zurücksetzen"):
-        Messstellen["Trend"] = np.nan
-        import os
-        if os.path.exists('./MKZ_Trend.csv'):
-            os.remove('./MKZ_Trend.csv')
-    if st.button("Trend berechnen:"):
-        first_notice = True
-        for z in MKZs:
-            if pd.isna(Messstellen.loc[(Messstellen["MKZ"]==z, "Trend")]).values[0]:
-            # if pd.isna(Messstellen["Trend"][Messstellen["MKZ"]==z]).values[0]:
-                if first_notice:
-                    st.write("Versuche Trendberechnung für "+str(len(MKZs))+" Messstellen seit "+str(Startdatum)+".")
-                    first_notice = False
-                               
-                if ((Messstellen.loc[
-                        Messstellen["MKZ"]==z, "Letztes_Messdatum"] <
-                        pd.to_datetime(Enddatum)).values[0]):
-                    st.warning("Zeitreihe zu kurz bei: "+z)
-                    continue
-        # Laden der Datenreihe
-                add = pd.read_csv('./cache/ExportSN_GWS-Rohdaten_'+z+'.csv',
-                                  sep=';',
-                                  thousands='.',
-                                  decimal=',',
-                 #               parse_dates=["MESSZEITPUNKT"],
-                 #              date_parser=dateparse,
-                 )
-                add['MESSZEITPUNKT'] = pd.to_datetime(add['MESSZEITPUNKT'], format='%Y-%m-%d')
-                add = add.set_index('MESSZEITPUNKT')
-                add = add.loc[:,"WERT_UNTER_GELAENDE"]
-                add = add.dropna()
-                add = add.loc[add.index>=pd.to_datetime(Startdatum)]
-                if(add.shape[0]<=0):
-                    st.warning("Keine Daten bei: "+z)
-                    continue
-    #            if 
-    
-    # Lineare Regression
-                x = (add.index - add.index[0]).days.values.reshape(-1, 1) / 365 # /365 ändert die Zeitwerte von Tag auf Jahr
-                y = add.values                                                  # Die WERT_UNTER_GELAENDE Werte sind in cm
-                a = linear_model.LinearRegression().fit(x, y)
-                linear_model.LinearRegression(copy_X=True, fit_intercept=True, n_jobs=1)
-    
-    #            import matplotlib.pyplot as plt
-    #            y_pred = a.predict([[x.min()],[x.max()]])
-    #            plt.plot(x,y)
-    #            plt.plot([x.min(),x.max()],y_pred)
-    #            plt.text(0,y.min(), a.coef_)
-    #            plt.show()
-    
-    # Berechnung Grimm Strele Trend
-                Messstellen.loc[(Messstellen["MKZ"]==z, "Trend")] = -a.coef_ / (y.max()-y.min()) *100 # Anstieg in cm/a / Spannweite der cm --> Grimm-Strele Test
-    
-        Messstellen.loc[:,("MKZ","Trend")].to_csv('./MKZ_Trend.csv',
-                                                   index = False)
-    #    st.write("Trendberechnung beendet.")
-    
-        #    Darstellung Histogram und Prozent
-        with c1c2:
-            trends = Messstellen["Trend"].copy()
-            trends = trends.dropna()
-            fig2 = px.histogram(Messstellen, x="Trend", height=250)
-            st.plotly_chart(fig2)
-            if trends.shape[0]>=1:
-                st.write("Anteil negativer Trend: "+
-                         str(round(len(trends[trends<=-2]) / len(trends) * 100))+
-                         "% von "+
-                         str(len(trends))+" Messstellen.")
+            if runtrendall:
+                Messstellen.to_csv("Trendanalyse_"+
+                                   datetime.datetime.now().strftime("%Y%m%d_%H%M")+
+                                   "_Start"+str(Startdatum)+
+                                   "_End"+str(Enddatum)+
+                                   "_Min"+str(Mindatum)+
+                                   ".csv")
