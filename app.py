2194eab4a83e56e94da16b0fc46caa4078f4a352
jonaslenz <38277721+jonaslenz@users.noreply.github.com>
2026-02-12 15:30:09 +0100

    to numeric Grimm-Strele

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ Messstellen = Messstellen.merge(Mess_GWK, on = "MKZ", how = "outer")
 
 Messstellen['Erstes_Messdatum'] = pd.to_datetime(Messstellen['Erstes_Messdatum'], format='%Y-%m-%d')
 Messstellen['Letztes_Messdatum'] = pd.to_datetime(Messstellen['Letztes_Messdatum'], format='%Y-%m-%d')
+Messstellen['GRIMM-STRELE'] = Messstellen['GRIMM-STRELE'].to_numeric()
 
 transformer = Transformer.from_crs("EPSG:25833", "EPSG:4326")
 lat, lon = transformer.transform(Messstellen.RW_ETRS89, Messstellen.HW_ETRS89)
@@ -318,4 +319,5 @@ with c2:
             my_bar.empty()
             st.write("Tabelle zum Download bereit.")
             st.rerun()
+
 
